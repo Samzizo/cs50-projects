692263b9a4ff36f777e383a9c6764fbbf22aa0d6
Samiha Amroune <68222563+Samzizo@users.noreply.github.com>
2026-09-24 23:13:41 +0300

    Add files via upload

---
+++ songs/6.sql
@@ -0,0 +1,1 @@
+SELECT name From songs WHERE artist_id = (SELECT id FROM artists WHERE name == "Post Malone");
---
+++ songs/7.sql
@@ -0,0 +1,1 @@
+SELECT AVG(energy) From songs WHERE artist_id = (SELECT id FROM artists WHERE name == "Drake");
